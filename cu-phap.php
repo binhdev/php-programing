7d14957ca248bf478b70ab31c170943497e29732
Ho Thi Hoai Quyen <hoaiquyen557@gmail.com>
2017-10-12 14:05:57 +0700

    cu phap php

---
+++ cu-phap.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>vi du chuong trinh php</title>
+</head>
+<body>
+  <?php
+  echo "vi du minh hao lenh echo!<br>";
+  print "vi du minh hoa lenh print";
+  ?>
+</body>
+</html>
