bd18f3f7f242018a1b31f4ccf827d82865be2f30
Anh Tuan <voanhtuan1129@gmail.com>
2017-10-12 16:36:13 +0700

    toan_tu_php

---
+++ toan_tu.php
@@ -0,0 +1,15 @@
+<?php
+  $a = 4;
+  $b = 2;
+  $Tong = $a + $b;
+  $Hieu = $a - $b;
+  $Tich = $a * $b;
+  $Thuong = $a / $b;
+  $Sodu = $a % $b;
+  $atang = ++$a;
+  $agiam = --$a;
+  echo "a = $a <br>";
+  echo "b = $b <br>";
+  echo "Tong = $Tong <br> Hieu = $Hieu <br> Tich = $Tich <br> Thuong = $Thuong";
+  echo "<br> So du = $Sodu <br> a++ = $atang <br> a-- = $agiam";
+ ?>
