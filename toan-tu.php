5485cec6e979fcc84359c010a3fe3b5ad30ce069
Anh Dung <nad79616@gmail.com>
2017-10-12 16:39:06 +0700

    toan tu trong php

---
+++ toan-tu.php
@@ -0,0 +1,24 @@
+<?php
+  $a = 8;
+  $b = 4;
+  $c = $a + $b;
+  $d = $a - $b;
+  $e = $a * $b;
+  $f = $a / $b;
+  $g = $a % $b;
+  $h = ++$a;
+  $i = --$a;
+  echo 'Tổng : '.$a .'+' .$b.'=' .$c .'<br>';
+  echo "******<br>";
+  echo 'Hiệu : '.$a .'-' .$b.'=' .$d .'<br>';
+  echo "******<br>";
+  echo 'Tích : '.$a .'*' .$b.'=' .$e .'<br>';
+  echo "******<br>";
+  echo 'Thương : '.$a .'/' .$b.'=' .$f .'<br>';
+  echo "******<br>";
+  echo 'Trả về số dư : '.$a .'%' .$b.'=' .$g .'<br>';
+  echo "******<br>";
+  echo 'Toán tử tăng của ' .$a .' là ' .$h  .'<br>';
+  echo "******<br>";
+  echo 'Toán tử giảm của ' .$h .' là ' .$i  .'<br>';
+?>
