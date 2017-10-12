9b5d6e8eb9159ad8ff0ca262844b2c77c6364ba1
letuankiet51 <letuankiet53@gmail.com>
2017-10-12 15:52:44 +0700

    them info

---
+++ bien_trong_php.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bien trong php</title>
+  </head>
+  <body>
+    <div style="border:solid 1px black;text-align: center;">
+      <?php
+        define("PI", 3.14);
+        $name="LE TUAN KIET";
+        $adess="10 Nguyen Hue";
+        $age ="20";
+        $lop ="15CDTH11";
+        echo("TEN:$name<br>");
+        echo("*************<br>");
+        echo("TEN:$adess <br>");
+        echo("*************<br>");
+        echo("TEN:$age<br>");
+        echo("*************<br>");
+        echo("TEN:$lop<br>");
+        echo("*************<br>");
+        echo("Pi =".PI);
+      ?>
+    </div>
+  </body>
+</html>
