bef7a0d8167732d22bbc1bac387113bbf26d380e
Quang Tran <quangtran1357@gmail.com>
2017-10-12 16:00:43 +0700

    Bien trong PHP

---
+++ bien.php
@@ -0,0 +1,18 @@
+<?php
+  define("PI", 3.14);
+  define('HOTGIRL', 'Gai dep');
+  $tuoi = "21";
+  $lop = "15CDTH11";
+  $name = "Trần Nhật Quang";
+  $gioitinh = "Nam";
+  //Ten, dia chi, tuoi, lop, gioi tinh
+  echo "Tên: $name <br>";
+  echo "*******************<br>";
+  echo "Tuổi: $tuoi <br>";
+  echo "*******************<br>";
+  echo "Lớp: $lop <br>";
+  echo "*******************<br>";
+  echo "Giới tính: $gioitinh <br>";
+  echo "*******************<br>";
+  echo HOTGIRL;
+?>
