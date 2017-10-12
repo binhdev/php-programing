00ccf43d80cb37580ed19f42072c710471b0aca2
nguyenhieulam <hieulam.ku97@gmai.com>
2017-10-12 16:03:35 +0700

    bien_trong_php

---
+++ bien.php
@@ -0,0 +1,13 @@
+<?php
+define("PI", 3.14);
+define("beatyful", 'gai xinh');
+
+$name ="Nguyễn Hiếu Lâm";
+$tuoi = 20;
+$address = "Huế";
+echo "Tên: $name <br>";
+echo "Tuổi: $tuoi <br>";
+echo "Địa chỉ: $address <br>";
+echo beatyful;
+
+?>
