446ecf5065051826bd6e87c826e9f94bb8b7b324
Quang Tran <quangtran1357@gmail.com>
2017-10-12 16:42:00 +0700

    Toan tu trong PHP

---
+++ toan_tu_trong_php.php
@@ -0,0 +1,24 @@
+<?php
+  $a=10;
+  $b=5;
+  $tong = $a + $b;
+  $hieu = $a - $b;
+  $tich = $a * $b;
+  $thuong = $a / $b;
+  $laydu = $a % $b;
+  $tttang = ++$a;
+  $ttgiam = --$b;
+  echo "Tổng là: $a + $b = $tong<br>";
+  echo "************************************<br>";
+  echo "Hiệu là: $a - $b = $hieu<br>";
+  echo "************************************<br>";
+  echo "Tích là: $a * $b = $tich<br>";
+  echo "************************************<br>";
+  echo "Thương là: $a / $b = $thuong<br>";
+  echo "************************************<br>";
+  echo "Chia lấy dư là: $a % $b = $laydu<br>";
+  echo "************************************<br>";
+  echo "Toán tử tăng a++ = $tttang<br>";
+  echo "************************************<br>";
+  echo "Toán tử giảm b-- = $ttgiam<br>";
+?>
