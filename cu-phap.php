8060cd64bfa7394b34716a3ee9e05164bf039bc6
Ho Thi Hoai Quyen <hoaiquyen557@gmail.com>
2017-10-12 14:24:25 +0700

    Them Cu Phap PHP

--- cu-phap.php
+++ cu-phap.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>vi du chuong trinh php</title>
-</head>
-<body>
-  <?php
-  echo "vi du minh hao lenh echo!<br>";
-  print "vi du minh hoa lenh print";
-  ?>
-</body>
+    <head>
+	    <title>vi du chuong trinh php</title>
+    </head>
+    <body>
+    <?php
+        echo "Vi du minh hao lenh echo!<br>";
+        print "Vi du minh hoa lenh print!";
+    ?>
+    </body>
 </html>
