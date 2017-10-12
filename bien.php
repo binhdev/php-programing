e13d352702178eae081b659ef952004830084848
Ho Thi Hoai Quyen <hoaiquyen557@gmail.com>
2017-10-12 16:00:36 +0700

    bien php

---
+++ bien.php
@@ -0,0 +1,12 @@
+<?php
+   define("PI",3.14);
+   define("HOTGIRL",'Gai dep');
+   $name = " Ho Thi Hoai Quyen ";
+   $address = "Hue";
+   $old = 15;
+   echo "Ten: $name <br>";
+   echo "Dia chi: $address <br>";
+   echo "Tuoi: $old <br>";
+   echo PI ;
+   echo HOTGIRL ;
+?>
