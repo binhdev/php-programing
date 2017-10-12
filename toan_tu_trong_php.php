2e60e58de70ac3c29d1eee4613ade0e0f3d10513
Ho Thi Hoai Quyen <hoaiquyen557@gmail.com>
2017-10-12 16:51:03 +0700

    toan tu trong php

---
+++ toan_tu_trong_php.php
@@ -0,0 +1,20 @@
+<?php
+   $a =9;
+   echo "So a : $a <br>";
+   $b =9;
+   echo "So b : $b <br>";
+   $tong = $a + $b;
+   echo "Tong : $a + $b =$tong <br>";
+   $hieu = $a - $b;
+   echo "Hieu : $a - $b =$hieu <br>";
+   $tich = $a * $b;
+   echo "Tich : $a * $b =$tich <br>";
+   $thuong = $a / $b;
+   echo "Thuong : $a / $b =$thuong <br>";
+   $tang = ++$a;
+   echo "Tang a 1 don vi : $tang <br>";
+   $giam = --$a;
+   echo "Giam a 1 don vi : $giam <br>";
+
+  
+?>
