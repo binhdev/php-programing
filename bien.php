5bb0c3f0704e1cdab791d230e6afaecfd78ab91b
Anh Tuan <voanhtuan1129@gmail.com>
2017-10-12 15:51:31 +0700

    bien php

---
+++ bien.php
@@ -0,0 +1,13 @@
+<?php
+  define("PI", 3.14);
+  define("HOTGIRL", "Gai dep");
+
+  $name = "Vo Anh Tuan";
+  $diachi = "185 Bui Thi Xuan";
+  $tuoi = "21";
+  $lop = "15CDTH11";
+  $gioitinh = "Nam";
+  echo "Ten : $name <br> Dia chi : $diachi <br> Tuoi : $tuoi <br> Lop : $lop <br> Gioi tinh : $gioitinh <br>";
+  echo "----------------- <br>";
+  echo HOTGIRL;
+ ?>
