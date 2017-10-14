b069d38e7409283bb6d3f92a2232a1cf3a91e623
khanh ngan <fafanguyen95@gmail.com>
2017-10-14 02:46:22 -0700

    toan_tu php

---
+++ Bien.php
@@ -0,0 +1,15 @@
+<?php
+define("PI", 3,14);
+define('HOTGIRL', 'khanh ngan');
+$name = "Ho Nguyen Khanh Ngan";
+$adress = "Hue";
+$old = 22;
+ echo "ten: $name <br>";
+ echo "*************";
+ echo "dia chi:adress <br>";
+ echo "*************";
+ echo "tuoi: $old <br>";
+ echo "*************";
+ echo HOTGIRL;
+ ?>
+
---
+++ toan_tu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+<?php
+  $a = 5;
+  $b = 6;
+  $tong = $a + $b;
+  echo " Tong cua $a +$b = $tong";
+  ?>
+</body>
+</html>
+
