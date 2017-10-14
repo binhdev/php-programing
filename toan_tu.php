33668e5604f4f4cd20425c52008e8433dd13698d
Pham Cong Luan <phamcongluan10011997@gmail.com>
2017-10-14 02:59:37 -0700

    toan_tu

--- toan_tu.php
+++ toan_tu.php
@@ -5,10 +5,26 @@
 </head>
 <body>
   <?php
-    $a = 5;
-    $b = 6;
+    $a = 100;
+
+    $b = 45;
+
     $tong = $a + $b;
-    echo "Tong cua $a + $b = $tong";
+
+    $hieu = $a - $b;
+
+    $tich = $a * $b;
+
+    $thuong = $a / $b;
+
+    echo "Tong cua $a + $b = $tong <br> ";
+
+    echo "Hieu cua $a - $b = $hieu <br>";
+
+    echo "Tich cua $a * $b = $tich <br>";
+
+    echo "Thuong cua $a / $b = $thuong <br>";
+
   ?>
 </body>
 </html>
