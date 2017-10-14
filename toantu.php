618aa9c94ec5a7bfbcd615436d82a95647c9611a
thinh <ducthinh12300@gmail.com>
2017-10-14 02:43:34 -0700

    toan tu php

---
+++ toantu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <?php
+    $a = 5;
+    $b = 6;
+    $tong = $a + $b ;
+    echo "Tong cua $a + $b = $tong";
+    $tich = $a * $b ;
+    echo "Tich cua $a * $b = $tich";
+    $thuong = $a / $b ;
+    echo "Thuong cua $a / $b = $thuong";
+  ?>
+</body>
+</html>
