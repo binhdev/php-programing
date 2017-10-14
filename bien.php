a57988a520410c6af0f451ff5282ecc1aaa8937b
paultranvanminh <paultranvanminh@gmail.com>
2017-10-14 15:09:42 +0700

    bien trong php

---
+++ bien.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>bien trong php</title>
+  </head>
+  <body>
+    <?php
+      define("PI", 3.14);
+      define('HOTGIRL', 'Gai dep');
+
+      $name = "Tran Van Minh";
+      $address = "Hue";
+      $old = 20;
+      echo "Ten: $name <br>";
+      echo "*******************<br>";
+      echo "Dia chi: $address <br>";
+      echo "*******************<br>";
+      echo "Tuoi: $old <br>";
+      echo "*******************<br>";
+      echo HOTGIRL;
+    ?>
+  </body>
+</html>
+
