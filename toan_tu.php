be1c30afac2a027ec4e727d52658c6ad5db6a560
vanlanh <lanhtran175@gmail.com>
2017-10-14 16:41:47 +0700

    Toan tu

--- toan_tu.php
+++ toan_tu.php
@@ -8,7 +8,13 @@
       $a = 5;
       $b = 6;
       $tong = $a + $b;
+      $hieu = $b - $a;
+      $tich = $a * $b;
+      $thuong = $a / $b;
       echo "Tong cua $a + $b = $tong <br>";
+      echo "Hieu cua $b - $a = $hieu <br>";
+      echo "Tich cua $a * $b = $tich <br>";
+      echo "Thuong cua $a / $b = $thuong <br>";
       echo "Van Lanh";
     ?>
   </body>
