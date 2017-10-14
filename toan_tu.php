cdc3ca8cc7be97d389ef0a048d91ee453ef5767e
vanlanh <lanhtran175@gmail.com>
2017-10-14 16:39:12 +0700

    Toan tu

---
+++ toan_tu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Toan tu</title>
+  </head>
+  <body>
+    <?php
+      $a = 5;
+      $b = 6;
+      $tong = $a + $b;
+      echo "Tong cua $a + $b = $tong <br>";
+      echo "Van Lanh";
+    ?>
+  </body>
+</html>
