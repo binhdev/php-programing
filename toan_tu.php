c9f88321d702b62ff75cf74a9fe8d205714c1d6a
Ngo Ta Bao Quang <choemmotdoi182@gmail.com>
2017-10-14 02:41:57 -0700

    toan tu php

---
+++ toan_tu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <?php
+    $a = 5;
+    $b = 6;
+    $tong = $a + $b;
+    echo "Tong cua $a + $b = $tong <br>";
+    $tich = $a * $b;
+    echo "Tich cua $a * $b = $tich <br>";
+    $thuong = $a / $b;
+    echo "Thuong cua $a / $b = $thuong <br>";
+  ?>
+
+</body>
+</html>
