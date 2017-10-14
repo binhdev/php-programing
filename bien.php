b0b19e8d7c15f600c0bbd700310794161057de7a
Ngo Ta Bao Quang <choemmotdoi182@gmail.com>
2017-10-14 01:06:38 -0700

    bien tronb php

---
+++ bien.php
@@ -0,0 +1,15 @@
+<?php
+  define("PI", 3,14);
+  define('HOTGIRL', 'Archer');
+
+  $name = "Ngo Ta Bao Quang";
+  $address = "Hue";
+  $old = 20;
+  echo "Ten: $name <br>";
+  echo "************************* <br>";
+  echo "Dia chi: $address <br>";
+  echo "************************* <br>";
+  echo "Tuoi: $old <br>";
+  echo "************************* <br>";
+  echo HOTGIRL;
+?>
