a246ddd22f9f8e9275387045f363b4a2be877098
vanlanh <lanhtran175@gmail.com>
2017-10-14 15:02:05 +0700

    bien-php

---
+++ bien.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bien trong PHP</title>
+  </head>
+  <body>
+    <?php
+      define("PI", 3.14);
+      define('HOTGIRL', 'Ngan tit');
+
+      $ten = "Tran Van Lanh";
+      $address = "Hue";
+      $old = 20;
+      echo "Ten: $ten <br>";
+      echo "************************** <br>";
+      echo "Dia chi: $address <br>";
+      echo "************************** <br>";
+      echo "Tuoi: $old <br>";
+      echo "************************** <br>";
+      echo HOTGIRL;
+    ?>
+  </body>
+</html>
