92d0cbde2ea36b8b0260178266e6d8af33f8e120
ngoc <paulhongngoc97@gmail.com>
2017-10-14 02:13:29 -0700

    Cu phap

---
+++ bien.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>bien trong php</title>
+</head>
+<body>
+    <?php
+      define("PI", 3,14);
+      define("HOTGIRL", 'Gai dep');
+
+      $name="Nguyen Hong Ngoc";
+      $address="Quang Binh";
+      $old=20;
+      echo "Ten:$name<br>";
+      echo "*******************<br>";
+      echo "Dia chi: $address <br>";
+      echo "*******************<br>";
+      echo "Tuoi: $old <br>";
+      echo "*******************<br>";
+      echo HOTGIRL;
+    ?>
+</body>
+</html>
