b992f517960782de398fbbea0bb1bb4aa3304070
thinh <ducthinh12300@gmail.com>
2017-10-14 01:12:12 -0700

    bien trong php

---
+++ bien.php
@@ -0,0 +1,12 @@
+<?php
+  define("PI",3.14);
+  define("HOTGIRL", 'Ngan tit');
+
+  $name = "NGUYEN DUC THINH ";
+  $address = "HUE";
+  $old = 30 ;
+  echo "Ten : $name <br>";
+  echo "Dia Chi : $address <br>";
+  echo "Tuoi : $old <br>";
+  echo HOTGIRL;
+?>
